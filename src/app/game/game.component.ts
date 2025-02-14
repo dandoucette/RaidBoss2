5bcaf74e0960d7f853f83c11c5ae6cbfb3dda0b3
Dan Doucette <dan_doucette@hotmail.com>
2025-02-14 13:43:06 -0500

    added card milling to boss

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -24,9 +24,11 @@ export class GameComponent implements OnInit {
   bossForm!: FormGroup;
   controlNames = {
     Damage: "damage",
-    Poison: "poison"
+    Poison: "poison",
+    CardsMilled: "cards-milled"
   }
   playerTurn: boolean = true;
+  bossCardsRemaining: number = 100;
 
   constructor(private route: ActivatedRoute, private service: BossService) { }
 
@@ -49,7 +51,8 @@ export class GameComponent implements OnInit {
   private initForm() {
     this.bossForm = new FormGroup({
       [this.controlNames.Damage]: new FormControl(0),
-      [this.controlNames.Poison]: new FormControl(0)
+      [this.controlNames.Poison]: new FormControl(0),
+      [this.controlNames.CardsMilled]: new FormControl(0)
     });
   }
 
@@ -63,10 +66,11 @@ export class GameComponent implements OnInit {
     this.playerTurn = false;
     this.bossHitPoints -= this.bossForm.get(this.controlNames.Damage)?.value;
     this.bossPoison += this.bossForm.get(this.controlNames.Poison)?.value;
+    this.bossCardsRemaining -= this.bossForm.get(this.controlNames.CardsMilled)?.value;
 
     this.diceRolls = [];
 
-    if (this.bossHitPoints <= 0 || this.bossPoison >= 10) {
+    if (this.bossHitPoints <= 0 || this.bossPoison >= 10 || this.bossCardsRemaining <= 0) {
       for (var i = 0; i < 10; i++) {
         const roll: Roll = {
           dieValue: 0,
@@ -94,7 +98,24 @@ export class GameComponent implements OnInit {
       }
 
       for (var i = 1; i <= rolls; i++) {
-        const dieRoll = this.rollaDie();
+        let duplicate: boolean = true;
+        var dieRoll: number = 0;
+        var exitCount: number = 0;
+
+        while(duplicate) {
+          dieRoll = this.rollaDie();
+          if(!this.diceRolls.find(x => x.dieValue == dieRoll)) {
+            duplicate = false;
+          }
+
+          // exit loop after too many tries
+          if (exitCount > 10) {
+            break;
+          }
+          exitCount++;
+        }
+
+        
         var ability = this.getBossAbilityFromDieRoll(dieRoll);
         var abilityText = this.processAbility(ability, rolls);
 
@@ -104,6 +125,8 @@ export class GameComponent implements OnInit {
         }
         this.diceRolls.push(roll);
       }
+
+      this.bossCardsRemaining -= rolls + 1;
     }
   }
 
@@ -143,10 +166,20 @@ export class GameComponent implements OnInit {
       text = text.replace("{xp}", String(amount));
     }
 
+    // check for a single amount
+    const fixedPattern = /\{\d{1}\}/;
+    if (fixedPattern.test(text)) {
+      var match = text.match(fixedPattern);
+      if (match != null) {
+        amount = Number(match[0].substring(1,2));
+        text = text.replace(match[0], String(amount));
+      }
+    }
+
     // checks for a formula pattern, i.e. {x*2}, {X+1}, {x/2}
-    const pattern = /\{x(\+|\*|\/)\d{1}\}/;
-    if(pattern.test(text)) {
-      var match = text.match(pattern);
+    const formulaPattern = /\{x(\+|\*|\/)\d{1}\}/;
+    if(formulaPattern.test(text)) {
+      var match = text.match(formulaPattern);
       if (match != null) {
         
         var operator = match[0].substring(2,3);
@@ -158,23 +191,45 @@ export class GameComponent implements OnInit {
           amount = rolls * constant;
         } else if (operator == "/") {
           // divide is always by 2, too hard to make it anything else
-          if (rolls % 2 == 0) {
-            amount = rolls / 2;
-          } else {
-            amount = (rolls + 1) / 2
-          }
+          amount = this.divideByTwo(rolls);
         }
 
         text = text.replace(match[0], String(amount));
       }
     }
 
+    // checks for a fixed amount multiplied by the players
+    const fixedAmountPattern = /\{\d{1}\*p\}/;
+    if (fixedAmountPattern.test(text)) {
+      var match = text.match(fixedAmountPattern);
+      if (match != null) {
+        var fixedAmount = Number(match[0].substring(1,2));
+        amount = fixedAmount * this.numberOfPlayers;
+        text = text.replace(match[0], String(amount));
+      }
+    }
+
+    // check for a random placeholder, this is signaled by an r with the bottom and top numbers
+    const randomAmountPattern = /\{r,\d{1},\d{1,2}\}/;
+    if (randomAmountPattern.test(text)) {
+      var match = text.match(randomAmountPattern);
+      if (match != null) {
+        var min = Number(match[0].substring(3,4));
+        var max = Number(match[0].substring(5,7).replace('}', ''));
+
+        let choice = Math.floor(Math.random() * (max - min) + min);
+
+        text = text.replace(match[0], String(choice));
+      }
+    }
+
     // sometimes the amount is multiplied by players
     // this is for that case
-    if (text.indexOf("{amount*p}")) {
+    if (text.indexOf("{amount*p}") > -1) {
       amount = amount * this.numberOfPlayers;
       text = text.replace("{amount*p}", String(amount));
     }
+
 
     // eventually could expand this for more boss effects
     if (ability.bossEffect != "") {
@@ -203,6 +258,8 @@ export class GameComponent implements OnInit {
         } else if (numberOfCreatures.indexOf("x+") > -1) {
           let equation = numberOfCreatures.split("+");
           creatures = rolls + Number(equation[1]);
+        } else if (numberOfCreatures == "x/2") {
+          creatures = this.divideByTwo(rolls);
         } else {
           creatures = Number(numberOfCreatures);
         }
@@ -246,6 +303,14 @@ export class GameComponent implements OnInit {
     let index = this.bossDefenders.findIndex(d => d.power == power);
     if (index > -1) {
       this.bossDefenders.splice(index, 1);
+    }
+  }
+
+  private divideByTwo(value: number) {
+    if (value % 2 == 0) {
+      return value / 2;
+    } else {
+      return (value + 1) / 2
     }
   }
 
